2bd4c827788aeda111d920c1748f58a118367eb5
leonardo batista <leonardobatista96@hotmail.com>
2025-03-31 19:17:40 -0300

    create modal to add new_transactions and delete transactions

--- finances/views.py
+++ finances/views.py
@@ -1,6 +1,6 @@
 import json
 from django.http import JsonResponse
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.db.models import Sum
 from django.views.decorators.csrf import csrf_exempt
@@ -18,38 +18,38 @@ class TransactionListView(ListView):
     context_object_name = 'transactions'
     paginate_by = 10
 
-    # def get_queryset(self):
-    #     transactions = super().get_queryset()
-    #     search = self.request.GET.get('search')
-    #     if search:
-    #         transactions = transactions.filter(
-    #             category__category__icontains=search)
-    #         return transactions
-    #     return Transaction.objects.all()
     def get_queryset(self):
         return Transaction.objects.all().order_by('-created_at')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['total_value'] = Transaction.objects.aggregate(
-            total_value=Sum('value')
-        )['total_value'] or 0
+        context["form"] = TransactionModelForm()
         return context
 
 
 class TransactionCreateView(CreateView):
     model = Transaction
     form_class = TransactionModelForm
-    template_name = 'new_transaction.html'
-    context_object_name = 'transaction'
     success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)  # Decodifica JSON
+            form = self.form_class(data)
+
+            if form.is_valid():
+                form.save()
+                return JsonResponse({"success": True})
+
+            return JsonResponse({"success": False, "errors": form.errors}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({"success": False, "error": "JSON inválido"}, status=400)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class TransactionUpdateView(UpdateView):
     model = Transaction
     form_class = TransactionModelForm
-    template_name = 'transaction_update.html'
 
     def get_success_url(self):
         return reverse_lazy('transaction-list')
@@ -85,33 +85,10 @@ class TransactionUpdateView(UpdateView):
 
 class TransactionDeleteView(DeleteView):
     model = Transaction
-    template_name = 'transaction_delete.html'
-    success_url = '/'
+    success_url = reverse_lazy()
 
-    def get(self, request, *args, **kwargs):
+    def delete(self, request, *args, **kwargs):
         transaction = get_object_or_404(Transaction, pk=self.kwargs['pk'])
-        return JsonResponse({
-            "id": transaction.id,
-            "description": transaction.description,
-            "category_id": transaction.category.id,
-            "category_name": transaction.category.category,
-            "account_id": transaction.account.id,
-            "account_name": transaction.account.name,
-            "value": transaction.value,
-            "created_at": transaction.created_at.strftime('%Y-%m-%d'),
-        })
+        transaction.delete()
 
-    def post(self, request, *args, **kwargs):
-        transaction = get_object_or_404(Transaction, pk=self.kwargs['pk]'])
-        data = json.loads(request.body)
-
-        transaction.description = data.get(
-            "description", transaction.description)
-        transaction.category_id = data.get("category", transaction.category.id)
-        transaction.account_id = data.get("account", transaction.account.id)
-        transaction.value = data.get("value", transaction.value)
-        transaction.created_at = data.get("created_at", transaction.created_at)
-
-        transaction.save()
-
-        return JsonResponse({"message": "Transacao excluida com sucesso"})
+        return JsonResponse({"success": "True"})
