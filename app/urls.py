17c36f1f53026f7cff992fbd9e761c8a0ca84c3c
leonardo batista <leonardobatista96@hotmail.com>
2025-04-01 22:07:06 -0300

    add sign-up

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from .views import SignUpView
 
 
 urlpatterns = [
@@ -24,6 +25,7 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', SignUpView.as_view(), name="register"),
 
     path('', include('finances.urls')),
 
---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import RegistrationForm
+
+
+class SignUpView(CreateView):
+    form_class = RegistrationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
