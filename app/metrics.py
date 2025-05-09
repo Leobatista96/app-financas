2c09a44a29c85d64c75a01c0df0b5b03a4efd73c
leob96 <leonardobatista96@hotmail.com>
2025-05-08 23:40:28 -0300

    fix error when categorie is None on metrics calculation

--- app/metrics.py
+++ app/metrics.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum
+from django.db.models import Sum, Count
 from finances.models import Transaction
 
 
@@ -6,17 +6,36 @@ def get_transactions_value():
     total_transactions_value = Transaction.objects.all(
     ).aggregate(Sum("value"))['value__sum']
 
+    check_category_is_none = Transaction.objects.aggregate(
+        Count('category'))['category__count']
+
     total_categories_revenues = Transaction.objects.filter(
         category__category__icontains='Receita').aggregate(Sum('value'))['value__sum']
 
     total_categories_expense = Transaction.objects.filter(
         category__category__icontains='Despesa').aggregate(Sum('value'))['value__sum']
 
+    if total_categories_expense is None:
+        return dict(
+            total_categories_expense=0,
+            total_transactions_value=total_transactions_value,
+            total_categories_revenues=total_categories_revenues,
+            balance=0,
+        )
+
+    if total_categories_revenues is None:
+        return dict(
+            total_categories_revenues=0,
+            total_transactions_value=total_transactions_value,
+            total_categories_expense=total_categories_expense,
+            balance=0,
+        )
+
     balance = total_categories_revenues - total_categories_expense
 
     return dict(
         total_transactions_value=total_transactions_value,
-        total_categories_revenue=total_categories_revenues,
+        total_categories_revenues=total_categories_revenues,
         total_categories_expense=total_categories_expense,
         balance=balance,
     )
