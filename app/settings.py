62323584fbfdd07477add8aee6fc77d53a83bfab
Leobatista96 <leonardobatista96@hotmail.com>
2025-09-12 13:33:13 -0300

    settings.py

--- app/settings.py
+++ app/settings.py
@@ -14,16 +14,16 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 
-DJANGO_ENV = config('DJANGO_ENV')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 
+DJANGO_ENV = config('DJANGO_ENV')
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
 if DJANGO_ENV == 'production':
