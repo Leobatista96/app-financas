7510fa41b106cdc29ea3382c899ba1bd7e6525c1
Leobatista96 <leonardobatista96@hotmail.com>
2025-09-12 15:26:41 -0300

    settings.py

--- app/settings.py
+++ app/settings.py
@@ -165,9 +165,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'app' / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'app' / 'static',
+# ]
 
 # Media files
 MEDIA_URL = '/media/'
